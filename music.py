ae276af19fc0e99af806840589c66620c465e0b7
Vitaliy Faleev <VitaliiFaleev@ya.ru>
2022-12-26 02:10:47 +0300

    Update music.py

--- music.py
+++ music.py
@@ -1,30 +1,4 @@
 import discord
-from discord.ext import commands
-import youtube_dl
-
-# Create an Intents object with the required permissions
-intents = discord.Intents.default()
-intents.members = True
-
-# Create a discord client and a voice client
-client = discord.Client(intents=intents)
-voice_client = None
-
-# Set the YouTube video URL
-video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-
-# Use youtube_dl to extract the audio from the YouTube video
-ydl_opts = {
-    "format": "bestaudio/best",
-    "postprocessors": [{
-        "key": "FFmpegExtractAudio",
-        "preferredcodec": "mp3",
-        "preferredquality": "192"
-    }]
-}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    info = ydl.extract_info(video_url, download=False)
-    audio_url = info["url"]
 
 # Function to play the audio from the YouTube video
 async def play_audio(voice_client):
@@ -33,58 +7,14 @@ async def play_audio(voice_client):
     # Play the audio
     voice_client.play(audio_source)
 
-# Event handler for when the client is ready
-@client.event
-async def on_ready():
-    # Create a global queue for video URLs
-    global queue
-    queue = []
-
-# Create an Intents object with the required permissions
-intents = discord.Intents.default()
-intents.members = True
-
-# Create a discord client and a command bot
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.command()
-async def join(ctx):
-    print("Received join command")  # Debugging message
-    # Get the voice channel to join
-    voice_channel = ctx.author.voice.channel
-    # Join the voice channel
-    global voice_client
-    voice_client = await voice_channel.connect()
-    # Play the audio from the first video in the queue
-    await play_audio(voice_client)
-
-# Command handler for the "leave" command
-@bot.command()
-async def leave(ctx):
-    # Disconnect from the voice channel
-    await voice_client.disconnect()
-
-# Command handler for the "volume" command
-@bot.command()
-async def volume(ctx, volume: int):
-    # Set the volume of the voice client
-    voice_client.source.volume = volume / 100
-
-# Command handler for the "add" command
-@bot.command()
-async def add(ctx, video_url: str):
-    # Extract the audio from the YouTube video
-    ydl_opts = {
-        "format": "bestaudio/best",
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192"
-        }]
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(video_url, download=False)
-        audio_url = info["url"]
-    # Add the audio URL to the queue
-    queue.append(audio_url)
+# Start the audio playback loop
+async def audio_loop():
+    while True:
+        # If the queue is not empty and the voice client is not playing audio
+        if queue and not voice_client.is_playing():
+            # Get the next audio URL from the queue
+            audio_url = queue.pop(0)
+            # Play the audio
+            await play_audio(voice_client)
+        # Sleep for a short period of time before checking the queue again
+        await asyncio.sleep(0.5)
